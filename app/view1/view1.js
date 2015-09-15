198832ceccb3591614709234301015d69d9f771d
Taylor <knives1230@gmail.com>
2015-09-15 00:08:19 -0400

    First commit

---
+++ app/view1/view1.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', function($scope) {
+
+		$scope.staff =	{
+			Employees:
+				[
+					{_id: 1, name: 'Bill', score: 0},
+					{_id: 2, name: 'Ted', score: 0},
+					{_id: 3, name: 'Mary', score: 0},
+					{_id: 4, name: 'Samantha', score: 0},
+					{_id: 5, name: 'Jordan', score: 0}
+				]
+		};
+
+		var employeeId = $scope.staff.Employees._id;
+
+		$scope.getId = function() {
+			console.log(employeeId);
+		};
+	}
+]);

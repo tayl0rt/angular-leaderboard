4a4b9cc65d1e18c25ae9ec1a7ade0973981e3382
Taylor <knives1230@gmail.com>
2015-11-24 23:42:24 -0500

    removed DB URI for security

--- config/dbURI.js
+++ config/dbURI.js
@@ -1,5 +1,6 @@
 //hold the URI in the db object - call it like normal: db.URI
 
 db = module.exports = {
-	 URI : 'mongodb://taylort:Bafmn420!@taylorserver.strangled.net:27017/meanLeaderboard'
+	//Add DB URI Here
+	URI : ''
 };
